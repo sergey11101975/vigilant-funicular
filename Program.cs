0ec60d0552976cc1d6318a3aedae0782aa9a8f8b
Sergey11101975 <bogoslovskiisv@yandex.ru>
2023-05-22 15:13:23 +0300

    семинар

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+/*
+Console.WriteLine("введите число");
+int number = int.Parse(Console.ReadLine());
+if (number < 1 | number > 4)
+{
+    Console.WriteLine("неверно");
+}
+if (number == 1)
+{
+    Console.WriteLine("понедельник");
+}
+if (number == 2)
+{
+    Console.WriteLine("вторник");
+}
+
+*/
+
+/* программа на вход принимает 1 число N а на выходе все целые числа от -N до +N
+
+int numberN = int.Parse(Console.ReadLine());
+int i = -numberN;
+
+while (i <= numberN){
+    Console.Write(i);
+    Console.Write(" ");
+    i += 1;
+}
+
+*/
+/* Принимает трёхзначное число и выдаёт последнюю цифру.
+*/
+
+int number = int.Parse(Console.ReadLine());
+if (number < 100 | number > 999)
+{
+    Console.WriteLine("ошибка");
+    return;
+}
+double result = (double)number % 10;
+
+Console.WriteLine(result);
+
